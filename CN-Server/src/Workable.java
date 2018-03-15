3061204ac51b8e6f17efec1e2192a1f7875e3ec3
Unknown <wiggelyjiggely@hotmail.com>
2018-03-15 17:17:33 +0100

    miss atomic bomb

--- CN-Server/src/Workable.java
+++ CN-Server/src/Workable.java
@@ -36,143 +36,152 @@ public class Workable implements Runnable {
 			System.out.println(Thread.currentThread().getName()); // prints out the current thread
 			BufferedReader request = new BufferedReader(new InputStreamReader(this.clientSocket.getInputStream()));
 			BufferedWriter response = new BufferedWriter(new OutputStreamWriter(this.clientSocket.getOutputStream()));
-			String requestedString = request.readLine(); // First line of the request
-			String header = requestedString + "\n";
-			String path = "";
-			String filetype = "";
-//			SimpleDateFormat sdf = new SimpleDateFormat(" EEE, dd MMM yyyy HH:mm:ss z");
-
-			if (requestedString == null) {
-				clientSocket.close(); // close connection to client when request is empty.
-				return;
-			}
-			else
-				path = requestedString.split(" ")[1].substring(0);
-			if (path.equals("/"))
-				path = "/index.html"; // change / to /index.html
-			if (requestedString.contains(".") && !path.isEmpty())
-				filetype = path.substring(path.lastIndexOf(".") + 1); // the type of the file
-
-			String command = requestedString.split(" ")[0]; // GET,PUT,HEAD,POST,DELETE,OPTIONS command
-//			Date modifiedSince =  new Date();
-			// input from client
-			while (!requestedString.equals("")) {
-				requestedString = request.readLine();
-				if (requestedString.equals("Connection: Close"))
-					close = true;
-			//	if (requestedString.startsWith("If-Modified-Since: ")) modifiedSince = sdf.parse(requestedString.substring(requestedString.lastIndexOf(" ") + 1));;
-				header = header + requestedString + "\n";
-			}
-			System.out.println(header);
-			Header head = new Header();
-
-			// HEAD command
-			if (command.equals("HEAD")) {
-				if (new File("../res" + path).exists())
-					head.setHeader(200, filetype, this.size, new File("../res" + path)); // file found
-				else
-					head.setHeader(404, filetype, this.size, null); // file not found
-
-				response.write(head.getHeader().toString());
-				response.write(head.getHeader().toString());
-			}
-
-			// PUT and POST command
-			else if (command.equals("PUT") || command.equals("POST")) {
-				String tempbody = request.readLine();
-				String body = tempbody + "\n";
-				while (!tempbody.isEmpty()) {
-					tempbody = request.readLine();
-					body = body + tempbody + "\n";
-				}
-				if (command.equals("PUT"))
-					head.setHeader(writeToServer(body, path), filetype, this.size, null); // PUT
-				else
-					head.setHeader(addToServer(body, path), filetype, this.size, null); // POST
-				response.write(head.getHeader().toString());
-			}
-
-			// file found and GET command
-			else if (command.equals("GET") && (new File("../res" + path).exists())) {
-				if (filetype.equals("jpg") || filetype.equals("png")) {
-					sendImage(new File("../res" + path), head, filetype, 200);
-				} else {
-					File HTMLfile = new File("../res" + path);
-					this.size = (int) HTMLfile.length();
-					head.setHeader(200, filetype, this.size, new File("../res" + path));
-					response.write(head.getHeader().toString());
-					response.write(HtmlToString(HTMLfile));
-				}
-			}
-
-			// file not found
-			else if (command.equals("GET") && (!new File("../res" + path).exists())) {
-				head.setHeader(404, filetype, this.size, null);
-				response.write(head.getHeader().toString());
-			}
-
-			// DELETE command and file found
-			else if (command.equals("DELETE")) {
-				if (new File("../res" + path).exists()) {
-					new File("../res" + path).delete();
-					head.setHeader(200, filetype, this.size, null);
-				} else {
-					head.setHeader(404, filetype, this.size, null);
-				}
-				response.write(head.getHeader().toString());
-			}
-
-			// OPTIONS returns the list of request methods supported
-			else if (command.equals("OPTIONS")) {
-				head.setHeader(201, filetype, this.size, null);
-				response.write(head.getHeader().toString());
-
-				StringBuilder options = new StringBuilder();
-				options.append("GET: A client can use the GET request to get a web resource from the server.\r\n");
-				options.append("HEAD: A client can use the HEAD request to get the header that a GET request would have obtained.\r\n");
-				options.append("POST: Used to post data up to the web server.\r\n");
-				options.append("PUT: Ask the server to store the data.\r\n");
-				options.append("DELETE: Ask the server to delete the data.\r\n");
-				options.append("OPTIONS: Ask the server to return the list of request methods it supports.\r\n");
-				response.write(options.toString());
-			}
-
-			// GETCOFFEE command
-			else if (command.equals("GETCOFFEE")) {
-				sendImage(new File("../res/img/teapot.png"), head, filetype, 418);
-			}
-
-			// Wrong method
-			else if (!command.equals("GET") || !command.equals("PUT") || !command.equals("POST")
-					|| !command.equals("HEAD") || !command.equals("OPTIONS") || !command.equals("DELETE")
-					|| !command.equals("GETCOFFEE")) {
-				head.setHeader(501, filetype, this.size, null);
-				response.write(head.getHeader().toString());
-			}
-
-			// client did something wrong
-			else {
-				head.setHeader(400, filetype, this.size, null);
-				response.write(head.getHeader().toString());
-			}
-			response.flush();
-			request.close();
+			WriteToClient wtc = new WriteToClient();
+			wtc.CreateHeader(request, response, clientSocket);
 			response.close();
-
-			// when everything fails consider it an internal server error
-		} catch (IOException ex) {
-			try {
-				Header head = new Header();
-				System.out.println(Thread.currentThread().getName());
-				sendImage(new File("../res/img/monkey.jpg"), head, "jpg", 500);
-			} catch (Exception e) {
-
-			}
-		}
+			request.close();
+		} catch (IOException e) {
+			e.printStackTrace();
+		} catch (ParseException ex) {
+			ex.printStackTrace();
+		}	
+//			String requestedString = request.readLine(); // First line of the request
+//			String header = requestedString + "\n";
+//			String path = "";
+//			String filetype = "";
+//			SimpleDateFormat sdf = new SimpleDateFormat(" EEE, dd MMM yyyy HH:mm:ss z");
+//
+//			if (requestedString == null) {
+//				clientSocket.close(); // close connection to client when request is empty.
+//				return;
+//			}
+//			else
+//				path = requestedString.split(" ")[1].substring(0);
+//			if (path.equals("/"))
+//				path = "/index.html"; // change / to /index.html
+//			if (requestedString.contains(".") && !path.isEmpty())
+//				filetype = path.substring(path.lastIndexOf(".") + 1); // the type of the file
+//
+//			String command = requestedString.split(" ")[0]; // GET,PUT,HEAD,POST,DELETE,OPTIONS command
+//			Date modifiedSince =  new Date();
+//			// input from client
+//			while (!requestedString.equals("")) {
+//				requestedString = request.readLine();
+//				if (requestedString.equals("Connection: Close"))
+//					close = true;
+//				if (requestedString.startsWith("If-Modified-Since: ")) modifiedSince = sdf.parse(requestedString.substring(requestedString.lastIndexOf(" ") + 1));;
+//				header = header + requestedString + "\n";
+//			}
+//			System.out.println(header);
+//			Header head = new Header();
+//
+//			// HEAD command
+//			if (command.equals("HEAD")) {
+//				if (new File("../res" + path).exists())
+//					head.setHeader(200, filetype, this.size, new File("../res" + path)); // file found
+//				else
+//					head.setHeader(404, filetype, this.size, null); // file not found
+//
+//				response.write(head.getHeader().toString());
+//				response.write(head.getHeader().toString());
+//			}
+//
+//			// PUT and POST command
+//			else if (command.equals("PUT") || command.equals("POST")) {
+//				String tempbody = request.readLine();
+//				String body = tempbody + "\n";
+//				while (!tempbody.isEmpty()) {
+//					tempbody = request.readLine();
+//					body = body + tempbody + "\n";
+//				}
+//				if (command.equals("PUT"))
+//					head.setHeader(writeToServer(body, path), filetype, this.size, null); // PUT
+//				else
+//					head.setHeader(addToServer(body, path), filetype, this.size, null); // POST
+//				response.write(head.getHeader().toString());
+//			}
+//
+//			// file found and GET command
+//			else if (command.equals("GET") && (new File("../res" + path).exists())) {
+//				if (filetype.equals("jpg") || filetype.equals("png")) {
+//					sendImage(new File("../res" + path), head, filetype, 200);
+//				} else {
+//					File HTMLfile = new File("../res" + path);
+//					this.size = (int) HTMLfile.length();
+//					head.setHeader(200, filetype, this.size, new File("../res" + path));
+//					response.write(head.getHeader().toString());
+//					response.write(HtmlToString(HTMLfile));
+//				}
+//			}
+//
+//			// file not found
+//			else if (command.equals("GET") && (!new File("../res" + path).exists())) {
+//				head.setHeader(404, filetype, this.size, null);
+//				response.write(head.getHeader().toString());
+//			}
+//
+//			// DELETE command and file found
+//			else if (command.equals("DELETE")) {
+//				if (new File("../res" + path).exists()) {
+//					new File("../res" + path).delete();
+//					head.setHeader(200, filetype, this.size, null);
+//				} else {
+//					head.setHeader(404, filetype, this.size, null);
+//				}
+//				response.write(head.getHeader().toString());
+//			}
+//
+//			// OPTIONS returns the list of request methods supported
+//			else if (command.equals("OPTIONS")) {
+//				head.setHeader(201, filetype, this.size, null);
+//				response.write(head.getHeader().toString());
+//
+//				StringBuilder options = new StringBuilder();
+//				options.append("GET: A client can use the GET request to get a web resource from the server.\r\n");
+//				options.append("HEAD: A client can use the HEAD request to get the header that a GET request would have obtained.\r\n");
+//				options.append("POST: Used to post data up to the web server.\r\n");
+//				options.append("PUT: Ask the server to store the data.\r\n");
+//				options.append("DELETE: Ask the server to delete the data.\r\n");
+//				options.append("OPTIONS: Ask the server to return the list of request methods it supports.\r\n");
+//				response.write(options.toString());
+//			}
+//
+//			// GETCOFFEE command
+//			else if (command.equals("GETCOFFEE")) {
+//				sendImage(new File("../res/img/teapot.png"), head, filetype, 418);
+//			}
+//
+//			// Wrong method
+//			else if (!command.equals("GET") || !command.equals("PUT") || !command.equals("POST")
+//					|| !command.equals("HEAD") || !command.equals("OPTIONS") || !command.equals("DELETE")
+//					|| !command.equals("GETCOFFEE")) {
+//				head.setHeader(501, filetype, this.size, null);
+//				response.write(head.getHeader().toString());
+//			}
+//
+//			// client did something wrong
+//			else {
+//				head.setHeader(400, filetype, this.size, null);
+//				response.write(head.getHeader().toString());
+//			}
+//			response.flush();
+//			request.close();
+//			response.close();
+//
+//			// when everything fails consider it an internal server error
+//		} catch (IOException ex) {
+//			try {
+//				Header head = new Header();
+//				System.out.println(Thread.currentThread().getName());
+//				sendImage(new File("../res/img/monkey.jpg"), head, "jpg", 500);
+//			} catch (Exception e) {
+//
+//			}
+//		}
 //		 catch (ParseException exx) {
 //			exx.printStackTrace();
 //		}
-		return;
+//		return;
 	}
 
 	/**
--- CN-Server/src/WriteToClient.java
+++ CN-Server/src/WriteToClient.java
@@ -19,62 +19,62 @@ public class WriteToClient {
 
 	}
 
-	public void CreateHeader(BufferedReader req, BufferedWriter res, Socket clientSocket) {
+	public void CreateHeader(BufferedReader req, BufferedWriter res, Socket clientSocket)
+			throws IOException, ParseException {
 		String requestedString;
-		try {
-			requestedString = req.readLine();
-			String header = requestedString + "\n";
-			String path = "";
-			String filetype = "";
-			SimpleDateFormat sdf = new SimpleDateFormat(" EEE, dd MMM yyyy HH:mm:ss z");
-			Date LastModifiedSince = new Date();
-			path = requestedString.split(" ")[1].substring(0); // path to file
-			if (path.equals("/"))
-				path = "/index.html"; // change / to /index.html
-			if (requestedString.contains(".") && !path.isEmpty())
-				filetype = path.substring(path.lastIndexOf(".") + 1); // the type of the file
-
-			String command = requestedString.split(" ")[0]; // GET,PUT,HEAD,POST,DELETE,OPTIONS command
-
-			// input from client
-			while (!requestedString.equals("")) {
-				requestedString = req.readLine();
-				if (requestedString.startsWith("if-Modified-since:")) {
-					LastModifiedSince = sdf.parse(requestedString.substring(requestedString.lastIndexOf(" ") + 1));
-				}
-				header = header + requestedString + "\n";
-			}
-			System.out.println(header);
-			Header head = new Header();
-			switch (command) {
-			case "HEAD": 
-				HeadRequest Headrequest = new HeadRequest(path, 0, LastModifiedSince, filetype, head);
-				res.write(Headrequest.getHeader().getHeader().toString());
-				res.write(Headrequest.getHeader().getHeader().toString());
-				break;
-			case "PUT":
-				PutRequest PutRequest = new PutRequest(req ,path, 0, LastModifiedSince, filetype, head);
-				res.write(PutRequest.getHeader().getHeader().toString());
-				break;
-			case "POST":
-				PostRequest PostRequest = new PostRequest(req ,path, 0, LastModifiedSince, filetype, head);
-				res.write(PostRequest.getHeader().getHeader().toString());
-				break;
-			case "GET":
-				GetRequest GetRequest = new GetRequest(req, path, 0, LastModifiedSince, filetype, head, clientSocket);
-				res.write(GetRequest.getHeader().getHeader().toString());
-				if (GetRequest.getBody() != null)res.write(GetRequest.getBody());
-				break;
-			case "GETCOFFEE":
-			case "DELETE":
-			case "OPTIONS":
-			}
-		} catch (IOException e) {
-
-			e.printStackTrace();
-		} catch (ParseException ex) {
-			ex.printStackTrace();
+		requestedString = req.readLine();
+		String header = requestedString + "\n";
+		String path = "";
+		String filetype = "";
+		SimpleDateFormat sdf = new SimpleDateFormat(" EEE, dd MMM yyyy HH:mm:ss z");
+		Date LastModifiedSince = new Date();
+		
+		if (requestedString == null) {
+			clientSocket.close(); // close connection to client when request is empty.
+			return;
 		}
+		
+		path = requestedString.split(" ")[1].substring(0); // path to file
+		if (path.equals("/"))
+			path = "/index.html"; // change / to /index.html
+		if (requestedString.contains(".") && !path.isEmpty())
+			filetype = path.substring(path.lastIndexOf(".") + 1); // the type of the file
 
+		String command = requestedString.split(" ")[0]; // GET,PUT,HEAD,POST,DELETE,OPTIONS command
+
+		// input from client
+		while (!requestedString.equals("")) {
+			requestedString = req.readLine();
+			if (requestedString.startsWith("If-Modified-Since: ")) {
+				LastModifiedSince = sdf.parse(requestedString.substring(requestedString.lastIndexOf(" ") + 1));
+			}	
+			header = header + requestedString + "\n";
+		}
+		System.out.println(header);
+		Header head = new Header();
+		switch (command) {
+		case "HEAD":
+			HeadRequest Headrequest = new HeadRequest(path, 0, LastModifiedSince, filetype, head);
+			res.write(Headrequest.getHeader().getHeader().toString());
+			res.write(Headrequest.getHeader().getHeader().toString());
+			break;
+		case "PUT":
+			PutRequest PutRequest = new PutRequest(req, path, 0, LastModifiedSince, filetype, head);
+			res.write(PutRequest.getHeader().getHeader().toString());
+			break;
+		case "POST":
+			PostRequest PostRequest = new PostRequest(req, path, 0, LastModifiedSince, filetype, head);
+			res.write(PostRequest.getHeader().getHeader().toString());
+			break;
+		case "GET":
+			GetRequest GetRequest = new GetRequest(req, path, 0, LastModifiedSince, filetype, head, clientSocket);
+			res.write(GetRequest.getHeader().getHeader().toString());
+			if (GetRequest.getBody() != null)
+				res.write(GetRequest.getBody());
+			break;
+		case "GETCOFFEE":
+		case "DELETE":
+		case "OPTIONS":
+		}
 	}
 }
