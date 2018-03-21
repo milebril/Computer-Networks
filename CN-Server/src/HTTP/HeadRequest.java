fd61ea38e835297ac7a4b7fda1b576d1404ae442
Unknown <wiggelyjiggely@hotmail.com>
2018-03-21 15:26:23 +0100

    heil

--- CN-Server/src/HTTP/HeadRequest.java
+++ CN-Server/src/HTTP/HeadRequest.java
@@ -21,11 +21,11 @@ public class HeadRequest {
 	 */
 	public HeadRequest(String path, int size, Date LastModifiedSince, String filetype, Header head) {
 		if (new File("../res" + path).exists()) {
-			if (LastModifiedSince == null || LastModifiedSince.before(new Date(new File("../res" + path).lastModified()))) {
-				head.setHeader(200, filetype,size, new File("../res" + path)); // file found
+			if (LastModifiedSince != null || LastModifiedSince.before(new Date(new File("../res" + path).lastModified()))) {
+				head.setHeader(304, filetype,size, new File("../res" + path)); // file found
 			}
 			else {
-				head.setHeader(304, filetype, size, new File("../res" + path)); // file found but not modified
+				head.setHeader(200, filetype, size, new File("../res" + path)); // file found but not modified
 			}																			
 		} else {
 			head.setHeader(404, filetype, size, new File("../res" + path)); // file not found
